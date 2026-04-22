002776e8151865e12cd257be2ffc061cfee71489
KapilTechnologies <kapiltech.applications@gmail.com>
2026-04-22 14:46:54 +0530

    db settings

--- backend/src/main/java/com/pgms/config/DataSourceConfig.java
+++ backend/src/main/java/com/pgms/config/DataSourceConfig.java
@@ -14,12 +14,23 @@ public class DataSourceConfig {
     public DataSource dataSource(
             @Value("${DB_URL}") String rawDbUrl,
             @Value("${DB_USERNAME}") String username,
-            @Value("${DB_PASSWORD}") String password
+            @Value("${DB_PASSWORD}") String password,
+            @Value("${DB_MAX_POOL_SIZE:2}") int maxPoolSize,
+            @Value("${DB_MIN_IDLE:0}") int minIdle,
+            @Value("${DB_CONNECTION_TIMEOUT_MS:30000}") long connectionTimeoutMs,
+            @Value("${DB_IDLE_TIMEOUT_MS:600000}") long idleTimeoutMs,
+            @Value("${DB_MAX_LIFETIME_MS:1800000}") long maxLifetimeMs
     ) {
         HikariDataSource dataSource = new HikariDataSource();
         dataSource.setJdbcUrl(normalizeJdbcUrl(rawDbUrl));
         dataSource.setUsername(username);
         dataSource.setPassword(password);
+        dataSource.setMaximumPoolSize(maxPoolSize);
+        dataSource.setMinimumIdle(minIdle);
+        dataSource.setConnectionTimeout(connectionTimeoutMs);
+        dataSource.setIdleTimeout(idleTimeoutMs);
+        dataSource.setMaxLifetime(maxLifetimeMs);
+        dataSource.setPoolName("pgms-hikari");
         return dataSource;
     }
 
